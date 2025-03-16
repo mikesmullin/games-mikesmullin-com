30aa091adc77b410c42bc843828d21cd1a47c536
Mike Smullin <mike@smullindesign.com>
2025-03-15 18:51:58 -0600

    update

--- publish.sh
+++ publish.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 set -x
 ls -1 | grep -v CNAME | grep -v publish.sh | xargs rm -rf
-wget -e robots=off -nH -m localhost:3000
+#wget -e robots=off -nH -m localhost:3000
+# from WSL2
+wget -e robots=off -nH -m 172.24.0.1:3000
+
+# git add -A
+# git commit -a -m update
+# git push
