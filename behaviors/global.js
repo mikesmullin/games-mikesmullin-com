7f1aa7bfc54b5429cb0307229a737cd1cb7eedbc
Mike Smullin <mike@mikesmullin.com>
2025-04-10 21:05:25 -0600

    update

---
+++ behaviors/global.js
@@ -0,0 +1,6 @@
+// Toggle iframe to full browser size
+var toggleIframeWidescreen;
+
+toggleIframeWidescreen = function() {
+  return document.body.classList.toggle('wide');
+};
--- publish.sh
+++ publish.sh
@@ -2,8 +2,10 @@
 set -x
 ls -1 | grep -v CNAME | grep -v publish.sh | xargs rm -rf
 #wget -e robots=off -nH -m localhost:3000
-# from WSL2
-wget -e robots=off -nH -m 172.24.0.1:3000
+# from WSL2 (if `npm start` was from Windows cmd prompt)
+#wget -e robots=off -nH -m 172.24.0.1:3000
+# from WSL2 (if `npm start` was also in WSL2)
+wget -e robots=off -nH -m 127.0.0.1:3000
 
 # git add -A
 # git commit -a -m update
